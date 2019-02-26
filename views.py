80ad2cefcab1c16e393e9d766518a40eef667037
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-26 18:10:49 -0500

    chore: Add chore login to connect to the db and add first route

---
+++ views.py
@@ -0,0 +1,20 @@
+from models import Base, User
+from flask import Flask, jsonify, request, url_for, abort
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+from flask import Flask
+
+engine = create_engine('sqlite:///users.db')
+
+@app.route('/users', methods=['POST'])
+
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=5000)
