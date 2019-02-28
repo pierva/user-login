55f81bc7829faf5f7b56f7741f43135a44209d2d
Piervalerio Vignola <piervalerio.vignola@gmail.com>
2019-02-27 20:18:58 -0500

    chore: Update user model to incldue OAuth 2.0

--- models.py
+++ models.py
@@ -13,9 +13,10 @@ secret_key = ''.join(random.choice(string.ascii_uppercase + string.digits)
 
 class User(Base):
     __tablename__ = 'user'
-
     id = Column(Integer, primary_key=True)
-    username = Column(String(32), index=True)
+    username = Column(String(32))
+    picture = Column(String)
+    email = Column(String, index=True)
     password_hash = Column(String(64))
 
     def hash_password(self, password):
